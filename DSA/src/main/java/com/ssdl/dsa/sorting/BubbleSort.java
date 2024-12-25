3e4cb0ec7c38ee3e0535ca6ddf8f59f3c826fb85
Sandar <sandar@solidplm.com>
2024-12-25 22:33:18 +0700

    add Bubble Sort

---
+++ DSA/src/main/java/com/ssdl/dsa/sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ssdl.dsa.sorting;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class BubbleSort implements SortingAlgorithm{
+    
+    @Override
+    public void sort(int[] arr)
+    {
+        for(int i = 0; i < arr.length; i++)
+        {
+            for(int j = 0; j < arr.length - i - 1; j++)
+            {
+                if(arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
