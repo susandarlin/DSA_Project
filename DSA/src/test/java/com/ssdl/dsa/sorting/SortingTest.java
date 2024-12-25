4cd19bcf7f3deb60f485881fba6e7737566755ed
Sandar <sandar@solidplm.com>
2024-12-25 22:41:33 +0700

    add SortingTest

---
+++ DSA/src/test/java/com/ssdl/dsa/sorting/SortingTest.java
@@ -0,0 +1,42 @@
+package com.ssdl.dsa.sorting;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.ssdl.dsa.util.Util;
+import org.junit.jupiter.api.Test;
+
+public class SortingTest {
+
+    Util util = new Util();
+    SortingAlgorithm sortingAlgo = new BubbleSort();
+
+    @Test
+    public void testEmptyElement() {
+        int[] arr = {};
+        sortingAlgo.sort(arr);
+        assertTrue(util.isArraySorted(arr));
+    }
+    
+    @Test
+    public void testSingleElement()
+    {
+        int[] arr = {3};
+        sortingAlgo.sort(arr);
+        assertTrue(util.isArraySorted(arr));
+    }
+    
+    @Test
+    public void testDoubleElement()
+    {
+        int[] arr = {3, 2};
+        sortingAlgo.sort(arr);
+        assertTrue(util.isArraySorted(arr));
+    }
+    
+    @Test
+    public void testBubbleSort()
+    {
+        int[] arr = {3, 2, 1};
+        sortingAlgo.sort(arr);
+        assertTrue(util.isArraySorted(arr));
+    }
+}
